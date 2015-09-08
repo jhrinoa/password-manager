5ebc22ee5adbeb34e4c0ab7ece4a92928ed7f30a
Jonathan <jonlee321@gmail.com>
2015-09-07 21:19:18 -0700

    - UI Router for navigation. - Initial login logic and form

--- src/main/java/com/jleepersonal/rest_api/AuthSvc.java
+++ src/main/java/com/jleepersonal/rest_api/AuthSvc.java
@@ -20,16 +20,21 @@ public class AuthSvc {
 	@Path("/login")
 	@Consumes(MediaType.APPLICATION_JSON)    
 	@Produces(MediaType.APPLICATION_JSON)
-	public User login2(User user) {
-		System.out.println("I AM CALLED2!");
-		
+	public User login(User user) {		
 		String pw = user.getPassword();
 		String username = user.getUsername();
 		
 		System.out.println("Logging in for username: " + username);
-
-		User res = new User(null, "username1", "password1");
 		
-		return res;
+		// TODO: Do a proper check against Mongo here.
+		// Also, try return 401 or something when failure.
+		if (username.equals("asd@asd.com") && pw.equals("zxc")) {
+			//TODO: create JWT and return it
+			User res = new User(null, "username1", "password1");
+			
+			return res;
+		}
+		
+		return null;
 	}
 }
--- src/main/resources/app.js
+++ src/main/resources/app.js
@@ -1,41 +1,91 @@
-var passwordMgrApp = angular.module('passwordMgrApp', ['ngRoute']);
+var passwordMgrApp = angular.module('passwordMgrApp', ['ui.router']);
 
 // configure our routes
-passwordMgrApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider
-        // route for the home page
-        .when('/passwordMgr', {
-            templateUrl : 'passwordMgr/pages/home.html',
-            controller  : 'mainController'
-        })
-
-        // route for the about page
-        .when('/passwordMgr/about', {
-            templateUrl : 'passwordMgr/pages/about.html',
-            controller  : 'aboutController'
-        })
-
-        // route for the contact page
-        .when('/passwordMgr/contact', {
-            templateUrl : 'passwordMgr/pages/contact.html',
-            controller  : 'contactController'
-        });
-    
-    $locationProvider.html5Mode(true);
-}]);
-
-
+passwordMgrApp.config(['$locationProvider', '$stateProvider', '$httpProvider',
+	function($locationProvider, $stateProvider, $httpProvider) {
+		$locationProvider.html5Mode({
+		    enabled: true,
+		    requireBase: false
+	    });
+		$stateProvider
+			// route for the home page
+			.state('home', {
+				url: '/',
+				templateUrl : 'pages/home.html',
+				controller : 'mainController'
+			})
+	
+			// route for the about page
+			.state('about', {
+				url: '/about',
+				templateUrl : 'pages/about.html',
+				controller : 'aboutController'
+			})
+	
+			// route for the contact page
+			.state('contact', {
+				url: '/contact',
+				templateUrl : 'pages/contact.html',
+				controller : 'contactController'
+			})
+	
+			// route for the contact page
+			.state('login', {
+				url: '/login',
+				templateUrl : 'pages/login.html',
+				controller : 'loginController'
+			});
+	}
+]);
 
 // create the controller and inject Angular's $scope
 passwordMgrApp.controller('mainController', function($scope) {
 	// create a message to display in our view
-	$scope.message = 'This is Home page!';
+	$scope.message = 'Keep it up!!!';
 });
 
 passwordMgrApp.controller('aboutController', function($scope) {
-    $scope.message = 'About what?';
+	$scope.message = 'About what?';
 });
 
 passwordMgrApp.controller('contactController', function($scope) {
-    $scope.message = 'Dont ever contact us!';
+	$scope.message = 'Dont ever contact us!';
 });
+
+passwordMgrApp.factory('Login', loginFactory);
+
+passwordMgrApp.controller('loginController', function($scope, $http, $state) {
+	console.log('jlee');
+	$scope.signIn = function() {
+		console.log('jlee-signin');
+		console.log('jlee-username:' + $scope.username);
+		console.log('jlee-pw:' + $scope.password);
+
+		// TODO: handle JWT
+		$http.post('rest/auth/login', {
+			username : $scope.username,
+			password : $scope.password
+		}).
+		then(
+			function (response) {
+				debugger;
+				console.log('Login successful!');
+				$state.go('home');
+			}, function  (err) {
+				console.log('Login failed');
+				$state.go('login');
+			}
+		);
+
+	}
+});
+
+function loginFactory($window) {
+	var isLoggedIn;
+	if ($window.localStorage.token) {
+		isLoggedIn = true;
+	}
+	return {
+		isLoggedIn : isLoggedIn
+	};
+}
